a676fe6b586f157829a3b9b5e47a4967d2640199
mamadAzimi <azimi.jozani.mohammad@gmail.com>
2023-11-28 15:28:31 +0330

    header just completed!

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -1,5 +1,5 @@
 import brandImg from '../images/brand.jpg'
-
+import { Link } from 'react-router-dom'
 
 function HomeBanner() {
   return (
@@ -9,8 +9,15 @@ function HomeBanner() {
             alt=""
             
         />
-        <div className='absolute top-0 flex items-center justify-center bg-opacity-40 bg-black h-full w-full'>
+        <div className='absolute gap-5 top-0 flex items-center flex-col justify-center bg-opacity-40 bg-black h-full w-full'>
+            <h1 className='text-white text-5xl font-bold'>hello world</h1>
             
+            <p className='text-white text-xl text-3xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, veniam?
+            </p>
+
+            <a href="/login" className='rounded-xl text-xl bg-red-700 capitalize text-white py-4 px-12 mt-8'>sign up</a>
+                {/* <Link to="/login"  /> */}
         </div>
     </div>
   )
